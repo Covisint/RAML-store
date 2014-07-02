ed276fc076872ce79a5f08c0ac42a0124aa7ebe3
Jake <jake.vanassche@gmail.com>
2014-07-02 11:23:07 -0400

    Refactored files.js and servers.js
    Completely removed Mongodb from all files
    
    --- Changes to files.js ---
    Made global var host for hostname
    Made global var filepath for path of requests
    Parsed datanode location of first request to do second request
    
    --- Changes to server.js ---
    Removed CheckAuth function

--- routes/files.js
+++ routes/files.js
@@ -1,42 +1,31 @@
 var http = require("http");
 
-var mongo = require('mongodb');
+// Global variables used in multiple functions
+var host = 'localhost';
+var userName;
+var filePath = '/webhdfs/v1/users/';
+var user_access_name = '&user.name=root';
 
-var Server = mongo.Server,
-  Db = mongo.Db,
-  BSON = mongo.BSONPure;
-
-var server = new Server('localhost', 27017, {auto_reconnect: true, journal: true, safe:false});
-db = new Db('ramldb', server);
-
-db.open(function (err, db) {
-  if (!err) {
-    console.log("Connected to 'ramldb' database");
-    db.collection('files', {strict: true}, function (err, collection) {
-      if (err) {
-        console.log("The 'files' collection doesn't exist. Use POST to add RAML files...");
-        populateDB();
-      }
-    });
-  }
-});
-
+// Variables used for extracting datanode location
+var datanode;
+var datanode_host;
+var datanode_port;
+var datanode_op;
 
 /**************************/
 /**   FINDING RAML FILE  **/
 /**************************/
 exports.findById = function (req, res) {
-	//var file = req.body.path;
-	console.log("id for file: " + req.params.id);
-	//console.log('Retrieving file: ' + file);
-	//console.log('req.params.path: ' + req.params.path);
+	console.log("FINDING RAML FILE");
+	var file = req.params.id;
+	console.log("file: " + file);
 
-  console.log("Sending request...");
-  // Options for request
-  var options1 = {
-		hostname: 'localhost',
+	console.log("Sending request...");
+	// Options for request
+	var options1 = {
+		hostname: host,
 		port: '50070',
-		path: '/webhdfs/v1/users/' + userName + '/' + req.params.id + '?op=OPEN',
+		path: filePath + userName + '/' + file + '?op=OPEN',
 		method: 'GET'
 		};
 
@@ -45,14 +34,10 @@ exports.findById = function (req, res) {
 		console.log("STATUS: " + res1.statusCode);
 		console.log("HEADERS: " + JSON.stringify(res1.headers));
 
-		/*res1.on('data', function (chunk) {
-				console.log('BODY: ' + chunk);
-			});*/
-
 		var options2 = {
 			hostname: 'localhost',
 			port: '50075',
-			path: '/webhdfs/v1/users/' + userName + '/' + req.params.id + '?op=OPEN&namenoderpcaddress=localhost:9000&user.name=root',
+			path: filePath + userName + '/' + file + '?op=OPEN&namenoderpcaddress=localhost:9000&user.name=root',
 			method: 'GET',
 		};
 
@@ -82,37 +67,7 @@ exports.findById = function (req, res) {
 	});
 
 	req1.end();	
-
-	/*if (req.params.id == "test2.raml")
-	{
-	
-item = {
-						"content":"#%25RAML%200.8%0Atitle:%20test!!!!!!"
 };
-	}*/
-	
-/*
-if(req.params.id == 'undefined' || req.params.id  === null){
-  	res.httpStatus = 404;
-    res.send(JSON.stringify({status: "error", response: "invalid id"}));
-  }
-  else{
-	  console.log("found id!");
- 	  var id = req.params.id;
- 	  db.collection('files', function (err, collection) {
- 	    collection.findOne({'_id': new BSON.ObjectID(id)}, function (err, item) {
- 	      delete item._id;
- 	      res.header("Access-Control-Allow-Origin", "*");
-		  console.log("ITEM: " + JSON.stringify(item));
- 	      res.send(item);
- 	    });
- 	  });
-  }*/
-
-};
-
-// Global variables used in multiple functions
-var userName;
 
 /**************************/
 /**  GETTING ALL FILES   **/
@@ -136,9 +91,9 @@ exports.findAll = function (req, res) {
 	var fileList = new Object();
 
 	var options1 = {
-			hostname: 'localhost',
+			hostname: host,
 			port: '50070',
-			path: '/webhdfs/v1/users/' + userName + '?op=LISTSTATUS',
+			path: filePath + userName + '?op=LISTSTATUS',
 			method: 'GET'
 	};
 
@@ -147,36 +102,31 @@ exports.findAll = function (req, res) {
 		console.log("STATUS: " + res1.statusCode);
 		console.log("HEADERS: " + JSON.stringify(res1.headers));
 
+		// Grabbing all of the path file names
 		res1.setEncoding('utf8');
 		res1.on('data', function (chunk) {
-				//console.log("ALL FILES");
-				//console.log('BODY: ' + chunk);
-
 				var split1 = chunk.split("pathSuffix\":\"");
+
 				// Start at 1 to avoid grabbing "FileStatuses"
 				for(var i = 1; i < split1.length; i++)
 				{
-					//console.log("path: " + res[i]);
 					var split2 = split1[i].split("\",");
 					
 					console.log(split2[0]);
 					paths.push(split2[0]);
-					
-					// for (var j = 0; j < res2.length; j++)
-					//{ console.log("path[" + i + ", " + j + "]: " + res2[j]); }
 				}
 
+				// Add each of the path names to fileList object
 				for (var i = 0; i < paths.length; i++)
 				{
-					console.log("path[" + i + "]:" + paths[i]);
+					//console.log("path[" + i + "]:" + paths[i]);
 					fileList[paths[i]] = {};
 					fileList[paths[i]]["path"] = "/" + paths[i];
 				}
-
-				console.log("FILELIST: " + JSON.stringify(fileList));
+				//console.log("FILELIST: " + JSON.stringify(fileList));
 				
+				// Send the fileList object
 				res.send(JSON.stringify({status: "ok", response: fileList}));
-
 			});
 	});
 
@@ -186,51 +136,6 @@ exports.findAll = function (req, res) {
 	});
 
 	req1.end();	
-
-	/*fileList1 = 
-		{
-			"Untitled-1.raml":
-				{	
-					"path":"/Untitled-1.raml",
-					//"content":"#%25RAML%200.8%0Atitle:%20test",
-					//"name":"Untitled-1.raml",
-					//"type":"file",
-					//"lastUpdated":"2014-06-30T16:32:10.771Z",
-					//"owner":"53b182233ede15d708dad618"
-				},
-			"test2.raml":
-				{
-					"path":"/test2.raml",
-						//"name":"test2.raml",
-						//"content":"#%25RAML%200.8%0Atitle:%20test2",
-						//"type":"file",
-						//"lastUpdated":"2014-06-30T18:10:06.542Z",
-						//"owner":"53b182233ede15d708dad618"
-				}
-		};
-
-		console.log("FILELIST1: " + JSON.stringify(fileList1));*/
-
-  /*db.collection('files', function (err, collection) {
-    collection.find({'owner': req.session.user_id}, function (err, resultCursor) {
-      resultCursor.each(function (err, item) {
-        if (item != null) {
-			console.log("ITEM: " + JSON.stringify(item));
-          console.log('Item : ' + item._id + ' : ' + item.path);
-          fileList[item._id] = item;
-          delete fileList[item._id]._id;
-         // console.log(JSON.stringify(fileList));
-                  }
-                          else {
-							  console.log("FILELIST: " + JSON.stringify(fileList));
-                                    res.header("Access-Control-Allow-Origin", "*");
-                                    res.send(JSON.stringify({status: "ok", response: fileList}));
-                          }
-                   });
-             });
-    });*/
-	//res.send(JSON.stringify({status: "ok", response: fileList}));
-          
 };
 
 
@@ -240,21 +145,6 @@ exports.findAll = function (req, res) {
 exports.addFile = function (req, res) {
 	console.log("Adding file to user: " + userName);
 
-
-/*var file = req.body;
-  console.log('Adding file : ' + JSON.stringify(file));
-  file.owner = req.session.user_id;
-  db.collection('files', function (err, collection) {
-    collection.insert(file, {safe: true}, function (err, result) {
-      if (err) {
-        res.send({'error': 'An error has occurred'});
-      } else {
-        console.log('Success: ' + JSON.stringify(result[0]));
-        res.header("Access-Control-Allow-Origin", "*");
-        res.send(result[0]);
-      }
-    });
-  });*/
 	// Getting the file path name from the request body
 	var file = req.body.path;
 
@@ -263,24 +153,30 @@ exports.addFile = function (req, res) {
 
 	// Options for first request
 	options1 = {
-		hostname: 'localhost',
+		hostname: host,
 		port: '50070',
-		path: '/webhdfs/v1/users/' + userName + file + '?op=CREATE',
+		path: filePath + userName + file + '?op=CREATE',
 		method: 'PUT'
-    	};
+    };
 
-	var datanode;
 	// FIRST REQUEST: Send intial request to redirect HDFS
 	var req1 = http.request(options1, function(res1) {
 		console.log("STATUS: " + res1.statusCode);
 		console.log("HEADERS: " + JSON.stringify(res1.headers));
+		
+		// Extracting location header and parsing it for second request
 		datanode = res1.headers['location'];
-		console.log("Location: " + datanode);
+		var sub = datanode.split("http://");
+		var firstColon = sub[1].indexOf(":");
+		var questionMark = sub[1].indexOf("?");
+		datanode_host = sub[1].substring(0, firstColon);
+		datanode_port = sub[1].substring(firstColon+1, sub[1].indexOf("/"));
+		datanode_op = sub[1].substring(questionMark);
 		
 		var options2 = {
-			hostname: 'localhost',
-			port: '50075',
-			path: '/webhdfs/v1/users/' + userName + file + '?op=CREATE&namenoderpcaddress=localhost:9000&overwrite=false&user.name=root',
+			hostname: datanode_host,
+			port: datanode_port,
+			path: filePath + userName + file + datanode_op + user_access_name,
 			method: 'PUT',
 		};
 
@@ -289,6 +185,77 @@ exports.addFile = function (req, res) {
 				console.log("STATUS: " + res2.statusCode);
 				console.log("HEADERS: " + JSON.stringify(res2.headers));
 
+				res2.on('data', function (chunk) {
+					console.log('BODY: ' + chunk);
+				});
+		});
+
+		req2.on('error', function(e) {
+				console.log("Problem with request2: " + e.message);
+		});
+	
+		// Write the contents to HDFS
+		req2.write(req.body.content);
+
+        req2.end();
+	});
+
+	// If an error occurs display problem
+	req1.on('error', function(e) {
+		console.log("Problem with request1: " + e.message);
+	});
+
+	req1.end();	
+
+};
+
+/**************************/
+/**  UPDATING RAML FILE  **/
+/**************************/
+exports.updateFile = function (req, res) {
+	console.log("UPDATING RAML FILE");
+
+	// Getting the file path name from the request body
+	var file = req.body.path;
+	console.log("File: " + file);
+
+	var options1 = {
+			hostname: host,
+			port: '50070',
+			path: filePath + userName + file + '?op=OPEN',
+			method: 'GET'
+			};
+
+	// FIRST REQUEST: Send initial request to redirect HDFS
+	var req1 = http.request(options1, function(res1) {
+		console.log("STATUS: " + res1.statusCode);
+		console.log("HEADERS: " + JSON.stringify(res1.headers));
+		
+		// Extracting location header and parsing it for second request
+		datanode = res1.headers['location'];
+		var sub = datanode.split("http://");
+		var firstColon = sub[1].indexOf(":");
+		var questionMark = sub[1].indexOf("?");
+		datanode_host = sub[1].substring(0, firstColon);
+		datanode_port = sub[1].substring(firstColon+1, sub[1].indexOf("/"));
+		datanode_op = sub[1].substring(questionMark);
+		// Remove '?op=OPEN', start string at first '&'
+		datanode_op = datanode_op.substring(datanode_op.indexOf("&"));
+		
+		var full_path = filePath + userName + file + datanode_op + "&overwrite=true" + user_access_name;
+		console.log("full_path: " + full_path);
+		
+		var options2 = {
+			hostname: datanode_host,
+			port: datanode_port,
+			path: filePath + userName + file + "?op=CREATE" + datanode_op + "&overwrite=true" + user_access_name,
+			method: 'PUT',
+		};
+
+		// SECOND REQUEST: Put file to HDFS
+		var req2 = http.request(options2, function(res2) {
+				console.log("STATUS: " + res2.statusCode);
+				console.log("HEADERS: " + JSON.stringify(res2.headers));
 				res2.on('data', function (chunk) {
 					console.log('BODY: ' + chunk);
 				});
@@ -308,71 +275,13 @@ exports.addFile = function (req, res) {
 		console.log("Problem with request1: " + e.message);
 	});
 
-	req1.end();	
-
-};
-
-exports.updateFile = function (req, res) {
-	console.log("Updating file for user: " + userName);
-
-
-	// Getting the file path name from the request body
-	var file = req.body.path;
-
-	console.log("File name: " + file);
-	console.log("Body: " + req.body.content);
-
-	var datanode;
-
-	var options1 = {
-			hostname: 'localhost',
-			port: '50070',
-			path: '/webhdfs/v1/users/' + userName + file + '?op=OPEN',
-			method: 'GET'
-			};
-
-	// FIRST REQUEST: Send initial request to redirect HDFS
-	var req1 = http.request(options1, function(res1) {
-		console.log("STATUS: " + res1.statusCode);
-		console.log("HEADERS: " + JSON.stringify(res1.headers));
-		datanode = res1.headers['location'];
-		console.log("Location: " + datanode);
-		
-			var options2 = {
-                hostname: 'localhost',
-                port: '50075',
-                path: '/webhdfs/v1/users/' + userName + file + '?op=CREATE&namenoderpcaddress=localhost:9000&overwrite=true&user.name=root',
-                method: 'PUT',
-        	};
-
-			// SECOND REQUEST: Put file to HDFS
-        	var req2 = http.request(options2, function(res2) {
-                	console.log("STATUS: " + res2.statusCode);
-                	console.log("HEADERS: " + JSON.stringify(res2.headers));
-			res2.on('data', function (chunk) {
-				console.log('BODY: ' + chunk);
-			});
-        	});
-
-        	req2.on('error', function(e) {
-                	console.log("Problem with request2: " + e.message);
-        	});
-	
-		req2.write(req.body.content);
-
-        	req2.end();
-	});
-
-	// If an errooccurs display problem
-	req1.on('error', function(e) {
-		console.log("Problem with request1: " + e.message);
-	});
-
 	req1.end();
 };
 
 
-//Deleting an Existing RAML File
+/**************************/
+/**  DELETING RAML FILE  **/
+/**************************/
 //Deletes from the database but not from the toolbar.. yet
 //code to update might be in index.html script
 exports.deleteFile = function (req, res) {
@@ -382,9 +291,9 @@ exports.deleteFile = function (req, res) {
     console.log('file: ' + file);
     //options for delete request
     var reqOps = {
-        hostname: 'localhost',
+        hostname: host,
         port: '50070',
-        path: '/webhdfs/v1/users/' + userName + '/' + file + '?op=DELETE&user.name=root',
+        path: filePath + userName + '/' + file + '?op=DELETE&user.name=root',
         method: 'DELETE',
     };
 
--- server.js
+++ server.js
@@ -3,15 +3,8 @@ var express = require('express'),
 files = require('./routes/files'),
 routes = require('./routes/');
  
-var util = require('./api-designer/scripts/sha1.js');
 
 var app = express();
-
-var mongo = require('mongodb');
-
-var Server = mongo.Server,
-  Db = mongo.Db,
-  BSON = mongo.BSONPure;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -48,12 +41,13 @@ app.configure('production', function(){
  * ------
  */
  
-app.get('/files', checkAuth, files.findAll);
-app.get('/files/:id', checkAuth, files.findById);
-app.post('/files', checkAuth, files.addFile);
-app.put('/files/:id', checkAuth, files.updateFile);
-app.delete('/files/:id', checkAuth, files.deleteFile);
-app.get('/', checkAuth, routes.index);
+//try to take out check auth
+app.get('/files', files.findAll);
+app.get('/files/:id', files.findById);
+app.post('/files', files.addFile);
+app.put('/files/:id', files.updateFile);
+app.delete('/files/:id', files.deleteFile);
+app.get('/', routes.index);
 
 app.get('/logout', function (req, res) {
   delete req.session.user_id;
@@ -65,45 +59,3 @@ app.get('/logout', function (req, res) {
 
 app.listen(app.get("port"));
 console.log('Listening on port 3000...');
-
-
-require("./mongo-http.js")
-
-function checkAuth(req, res, next) {
-	
-/** Getting username from Nginx Authorization **/
-	var headers = req.header("authorization");
-	// Remove BASIC tag
-	var sub = headers.substring(6, headers.length)
-	// Decode header from base 64 to a string
-	var buffer = new Buffer(sub, 'base64');
-	var decoded = buffer.toString();
-	// Split and grab the Username
-	var index = decoded.indexOf(':');
-	var userName = decoded.substring(0, index);
-	console.log("userName: " + userName);
-
-/** CHECKING IF USER IS IN MONGODB **/
-  db.collection('users', function (err, collection) {
-        collection.findOne({'mail': userName}, function (err, item) {
-			// Item not found or error has occured
-			if(!item || err){
-			  console.log("error: " + err);
-            }
-			else{
-				req.session.user_id = item._id;
-				console.log("user_id1: " + req.session.user_id);
-				if (!req.session || !req.session.user_id) {
-					res.statusCode = 401;
-					res.send({status:"error", message:"You are not authorized to view this page"});
-				} 
-				else {
-					next();
-				}
-			}
-        });
-	});
-
-}
-
-
